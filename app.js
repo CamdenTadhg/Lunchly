c45ff7c9d1d80d589b41e1eea574dcc236486482
camdentadhg <camdent@gmail.com>
2024-05-25 08:04:57 -0500

    full name method

--- app.js
+++ app.js
@@ -36,3 +36,21 @@ app.use((err, req, res, next) => {
 });
 
 module.exports = app;
+
+// 2) add fullName method
+// 1) write save reservations method
+
+// 14) add search functionality
+// 13) add functionality to display best customers
+// 12) learn about getters and setters
+// 11) create hidden notes functionality
+// 10) turn fullName into a getter
+// 9) create a getter/setter for numGuests
+// 8) create a getter/setter for startAt
+// 7) create a getter/setter for customerId
+// 6) add a field for middle name
+// 5) add a feature to edit existing reservations
+// 4) show most recent reservation for customers on customer listing page (should also show customers with no reservations)
+// 3) Learn about moment.js and change customer detail page to show most recent reservation with pretty version
+// 2) write tests for a template-based Express app
+// 1) add full-text search
